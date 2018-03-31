0d497619fc50c7458720199594f669f3437644db
Todd Rimes <todd@rimesmedia.com>
2018-03-31 16:25:39 -0700

    completed stubbed functions
    matrix is solved first time, returned from cache susbequently

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrix <- NULL
+        set <- function(y) {
+                x <<- y
+                matrix <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(x) matrix <<- solve(x)
+        getmatrix <- function() matrix
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'.
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached already-inverted matrix")
+                return(m)
+        }
+        data <- x$get()
+        x$setmatrix(data)
+        x$getmatrix()
 }
